3022008e3e51862393f70c814e31fef9b4093b1b
Per Starke <per@starke-team.de>
2025-09-08 11:31:35 +1000

    70/

--- Assets/Scripts/InputManager.cs
+++ Assets/Scripts/InputManager.cs
@@ -3,8 +3,7 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class InputManager : MonoBehaviour
-{    
-    
+{
     [SerializeField] private GameObject item; 
     private Tweener tweener;
 
@@ -13,9 +12,47 @@ public class InputManager : MonoBehaviour
         tweener = GetComponent<Tweener>();
     }
 
-    // Update is called once per frame
     void Update()
     {
-        
+        if (Input.GetKeyDown(KeyCode.A))
+        {
+            tweener.AddTween(
+                item.transform,
+                item.transform.position,
+                new Vector3(-2.0f, 0.5f, 0.0f),
+                1.5f
+            );
+        }
+
+        if (Input.GetKeyDown(KeyCode.D))
+        {
+            tweener.AddTween(
+                item.transform,
+                item.transform.position,
+                new Vector3(2.0f, 0.5f, 0.0f),
+                1.5f
+            );
+        }
+
+        if (Input.GetKeyDown(KeyCode.S))
+        {
+            tweener.AddTween(
+                item.transform,
+                item.transform.position,
+                new Vector3(0.0f, 0.5f, -2.0f),
+                0.5f
+            );
+        }
+
+        if (Input.GetKeyDown(KeyCode.W))
+        {
+            tweener.AddTween(
+                item.transform,
+                item.transform.position,
+                new Vector3(0.0f, 0.5f, 2.0f),
+                0.5f
+            );
+        }
     }
 }
+
