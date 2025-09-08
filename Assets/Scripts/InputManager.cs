5f8ca3a2a6734068a881952fe2fdfd286c6c2d49
Per Starke <per@starke-team.de>
2025-09-08 11:43:56 +1000

    100%

--- Assets/Scripts/InputManager.cs
+++ Assets/Scripts/InputManager.cs
@@ -1,56 +1,55 @@
 using UnityEngine;
+using System.Collections.Generic;
 
 public class InputManager : MonoBehaviour
 {
     [SerializeField] private GameObject item; 
     private Tweener tweener;
+    private List<GameObject> itemList = new List<GameObject>();
 
     void Start()
     {
         tweener = GetComponent<Tweener>();
+        itemList.Add(item);
     }
 
     void Update()
     {
+        // Spacebar - clone the box
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            GameObject clone = Instantiate(item, new Vector3(0f, 0.5f, 0f), Quaternion.identity);
+            itemList.Add(clone);
+        }
+
+        // movement keys - try to tween one box
         if (Input.GetKeyDown(KeyCode.A))
         {
-            tweener.AddTween(
-                item.transform,
-                item.transform.position,
-                new Vector3(-2.0f, 0.5f, 0.0f),
-                1.5f
-            );
+            TryTween(new Vector3(-2.0f, 0.5f, 0.0f), 1.5f);
         }
-
         if (Input.GetKeyDown(KeyCode.D))
         {
-            tweener.AddTween(
-                item.transform,
-                item.transform.position,
-                new Vector3(2.0f, 0.5f, 0.0f),
-                1.5f
-            );
+            TryTween(new Vector3(2.0f, 0.5f, 0.0f), 1.5f);
         }
-
         if (Input.GetKeyDown(KeyCode.S))
         {
-            tweener.AddTween(
-                item.transform,
-                item.transform.position,
-                new Vector3(0.0f, 0.5f, -2.0f),
-                0.5f
-            );
+            TryTween(new Vector3(0.0f, 0.5f, -2.0f), 0.5f);
         }
-
         if (Input.GetKeyDown(KeyCode.W))
         {
-            tweener.AddTween(
-                item.transform,
-                item.transform.position,
-                new Vector3(0.0f, 0.5f, 2.0f),
-                0.5f
-            );
+            TryTween(new Vector3(0.0f, 0.5f, 2.0f), 0.5f);
+        }
+    }
+
+    // loop over all items and try to start a tween
+    private void TryTween(Vector3 endPos, float duration)
+    {
+        foreach (var obj in itemList)
+        {
+            if (tweener.AddTween(obj.transform, obj.transform.position, endPos, duration))
+            {
+                break; // only assign one tween
+            }
         }
     }
 }
-
--- Assets/Scripts/Tweener.cs
+++ Assets/Scripts/Tweener.cs
@@ -1,44 +1,70 @@
 using UnityEngine;
+using System.Collections.Generic;
 
 public class Tweener : MonoBehaviour
 {
-    private Tween activeTween;
+    // private Tween activeTween; // no longer needed
+    private List<Tween> activeTweens = new List<Tween>();
+
+    // check if transform already has an active tween
+    public bool TweenExists(Transform target)
+    {
+        foreach (var tween in activeTweens)
+        {
+            if (tween.Target == target)
+            {
+                return true;
+            }
+        }
+        return false;
+    }
+
+    // try to add a new tween
+    public bool AddTween(Transform targetObject, Vector3 startPos, Vector3 endPos, float duration)
+    {
+        if (!TweenExists(targetObject))
+        {
+            activeTweens.Add(new Tween(targetObject, startPos, endPos, Time.time, duration));
+            return true;
+        }
+        return false;
+    }
 
     void Update()
     {
-        if (activeTween != null && activeTween.Target != null)
+        // temporary list to mark finished tweens
+        List<Tween> finishedTweens = new List<Tween>();
+
+        foreach (var tween in activeTweens)
         {
-            // distance check
-            float distance = Vector3.Distance(activeTween.Target.position, activeTween.EndPos);
+            if (tween.Target == null) continue;
+
+            float distance = Vector3.Distance(tween.Target.position, tween.EndPos);
 
             if (distance > 0.1f)
             {
                 // fraction of journey completed
-                float timeFraction = (Time.time - activeTween.StartTime) / activeTween.Duration;
+                float timeFraction = (Time.time - tween.StartTime) / tween.Duration;
 
-                // cubic easing-in (slower start, faster finish)
+                // cubic easing-in
                 timeFraction = Mathf.Pow(timeFraction, 3);
 
-                // lerp from startPos to endPos based on eased fraction
-                Vector3 newPos = Vector3.Lerp(activeTween.StartPos, activeTween.EndPos, timeFraction);
-
-                activeTween.Target.position = newPos;
+                // lerp from startPos to endPos
+                Vector3 newPos = Vector3.Lerp(tween.StartPos, tween.EndPos, timeFraction);
+                tween.Target.position = newPos;
             }
             else
             {
-                // snap to final position and clear tween
-                activeTween.Target.position = activeTween.EndPos;
-                activeTween = null;
+                // snap to final and mark tween as finished
+                tween.Target.position = tween.EndPos;
+                finishedTweens.Add(tween);
             }
         }
-    }
 
-
-    public void AddTween(Transform targetObject, Vector3 startPos, Vector3 endPos, float duration)
-    {
-        if (activeTween == null)
+        // remove all finished tweens
+        foreach (var t in finishedTweens)
         {
-            activeTween = new Tween(targetObject, startPos, endPos, Time.time, duration);
+            activeTweens.Remove(t);
         }
     }
 }
