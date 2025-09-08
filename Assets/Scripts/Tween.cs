d0aa281f67a817ac7e4295304d8de4dd8c816f36
Per Starke <per@starke-team.de>
2025-09-08 11:24:22 +1000

    50%

---
+++ Assets/Scripts/Tween.cs
@@ -0,0 +1,13 @@
+using UnityEngine;
+
+public class Tween
+{
+    public Transform Target { get; private set; }
+    public Vector3 StartPos { get; private set; }
+    public Vector3 EndPos { get; private set; }
+    public float StartTime { get; private set; }
+    public float Duration { get; private set; }
+
+    // Empty constructor for now
+    public Tween() { }
+}
